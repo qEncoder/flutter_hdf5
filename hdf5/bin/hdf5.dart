ea5fa00b34db2166e373086cd9ece38c301b77d0
Stephan Philips <s.g.j.philips@tudelft.nl>
2023-07-11 15:13:01 +0200

    stephan desktop old things

---
+++ hdf5/bin/hdf5.dart
@@ -0,0 +1,5 @@
+import 'package:hdf5/hdf5.dart' as hdf5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hdf5.calculate()}!');
+}
---
+++ hdf5/test/hdf5_test.dart
@@ -0,0 +1,4 @@
+import 'package:hdf5/hdf5.dart';
+import 'package:test/test.dart';
+
+void main() {}

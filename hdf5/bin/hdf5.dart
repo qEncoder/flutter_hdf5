27474018a13436b60d758a1ded5b6c09158a55fe
Stephan Philips <s.g.j.philips@tudelft.nl>
2023-07-11 15:17:22 +0200

    cleaning

--- hdf5/bin/hdf5.dart
+++
@@ -1,5 +0,0 @@
-import 'package:hdf5/hdf5.dart' as hdf5;
-
-void main(List<String> arguments) {
-  print('Hello world: ${hdf5.calculate()}!');
-}
--- hdf5/test/hdf5_test.dart
+++
@@ -1,4 +0,0 @@
-import 'package:hdf5/hdf5.dart';
-import 'package:test/test.dart';
-
-void main() {}

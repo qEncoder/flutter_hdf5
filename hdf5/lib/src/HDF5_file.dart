800878d77eef39f02c09b53a47e63336545e5a68
Stephan Philips <honger.meer.honger@gmail.com>
2024-01-18 11:12:31 +0100

    fixes hdf5 lib

--- hdf5/lib/src/HDF5_file.dart
+++ hdf5/lib/src/HDF5_file.dart
@@ -7,20 +7,14 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 class H5File implements Finalizable {
-  final String fileName
+  final String fileName;
   late final int fileId;
 
   static final _finalizer = NativeFinalizer(HDF5Bindings().H5F.closePtr);
 
-<<<<<<< HEAD
   H5File.open(this.fileName) {
     Pointer<Uint8> namePtr = strToChar(fileName);
-    fileId = HDF5Bindings().H5F.open(namePtr, H5F_ACC_RDONLY, H5P_DEFAULT);
-=======
-  H5File.open(String filename) {
-    Pointer<Uint8> namePtr = strToChar(filename);
     fileId = HDF5Bindings().H5F.open(namePtr, H5F_ACC_SWMR_READ, H5P_DEFAULT);
->>>>>>> origin/liveplotting
     calloc.free(namePtr);
 
     _finalizer.attach(this, Pointer.fromAddress(fileId));

afbbf0836e3483a648cf4ae2dfda78d093b4c746
Stephan Philips <s.g.j.philips@tudelft.nl>
2024-02-28 09:30:48 +0100

    fix compile errors for windows

---
+++ hdf5/example/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\steph\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\steph\\coding\\databrowser\\hdf5\\example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\steph\\flutter"
+  "PROJECT_DIR=C:\\Users\\steph\\coding\\databrowser\\hdf5\\example"
+  "FLUTTER_ROOT=C:\\Users\\steph\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\steph\\coding\\databrowser\\hdf5\\example\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\steph\\coding\\databrowser\\hdf5\\example"
+  "FLUTTER_TARGET=C:\\Users\\steph\\coding\\databrowser\\hdf5\\example\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMmI2MDBmMjQwNTQ4YzFjZjZmNWQzZjlhZTY1YTVjOWU1MWJjNDQzLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\steph\\coding\\databrowser\\hdf5\\example\\.dart_tool\\package_config.json"
+)
